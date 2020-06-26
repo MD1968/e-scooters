8346c77fe0b0c1bd356459fb95ea4f2f55d9e332
Mark Dyson <mark.dyson1@outlook.com>
2020-06-26 06:52:40 +0000

    url and rating fields removed

--- products/models.py
+++ products/models.py
@@ -27,9 +27,6 @@ class Product(models.Model):
     km = models.DecimalField(max_digits=3,decimal_places=0, null=True, blank=True)
     time = models.DecimalField(max_digits=3,decimal_places=0, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
-                                 blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
